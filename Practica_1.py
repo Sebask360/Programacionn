023f17873668f02fe51225bb4c91498390c5d39d
Sebask360 <sebastiancusism@gmail.com>
2024-07-15 18:38:47 -0500

    Practica 1 Commit 1

---
+++ Practica_1.py
@@ -0,0 +1,33 @@
+#Practica Print 1
+print("Me encanta estudiar python")
+print("\n")
+
+#Practica Print 2
+print("Estudiar con \"Python Total\" es super divertido")
+print("\n")
+
+#Practica Print 3
+print(500 + 50 + 5)
+print("\n")
+
+#Practica String 1
+print("Linea 1" "\n" "Linea 2" "\n" "Linea 3" ) 
+print("\n")
+
+#Practica String 2
+print(" A \t B \t C \n D \t E \t F \n G \t H \t I ")
+print("\n")
+
+#Practica Input 1
+
+input("Dime que estas estudiando ")
+print("Tu estas estudiando " + input("" "\n"))
+print("\n")
+
+#Practica Input 2
+input("¿En que pais vives?")
+print("Tu vives en " + input("" "\n"))
+print("\n")
+
+#Practica Input 3
+print("\n")
