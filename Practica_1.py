e126283a4967402754a8c3db292e43d05a22d4d9
Sebask360 <sebastiancusism@gmail.com>
2024-07-16 01:20:20 -0500

    Practica 1 Commit 2

--- Practica_1.py
+++ Practica_1.py
@@ -1,32 +1,26 @@
 #Practica Print 1
-print("Me encanta estudiar python")
-print("\n")
+print("Me encanta estudiar python \n")
 
 #Practica Print 2
-print("Estudiar con \"Python Total\" es super divertido")
-print("\n")
+print("Estudiar con \"Python Total\" es super divertido \n")
 
 #Practica Print 3
-print(500 + 50 + 5)
-print("\n")
+print(500 + 50 + 5, "\n")
 
 #Practica String 1
-print("Linea 1" "\n" "Linea 2" "\n" "Linea 3" ) 
-print("\n")
+print("Linea 1" "\n" "Linea 2" "\n" "Linea 3 \n" ) 
 
 #Practica String 2
-print("A\tB\tC\nD\tE\tF\nG\tH\tI")
-print("\n")
+print("A\tB\tC\nD\tE\tF\nG\tH\tI \n")
 
 #Practica Input 1
-estudio = input("Dime que estas estudiando ")
-print("Tu estas estudiando " + input("" "\n"))
-print("\n")
+estudio = input("Dime que estas estudiando \n")
 
 #Practica Input 2
-pais = input("¿En que pais vives?")
-print("Tu vives en " + input("" "\n"))
+pais = input("¿En que pais vives? \n")
 print("\n")
 
+print(f"Tu vives en {pais}")
+print(f"Tu estas estudiando {estudio}\n")
+
 #Practica Input 3
-print("\n")
