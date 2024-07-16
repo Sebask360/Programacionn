0261451ad9286eb439ba054dcdea9cbd702f6dea
Sebask360 <sebastiancusism@gmail.com>
2024-07-16 02:24:25 -0500

    Realice las practicas, pero creo que en las de conversion no me resulto como debia.

--- Practica_1.py
+++ Practica_1.py
@@ -38,15 +38,40 @@ edad = 51
 
 nombre = "Julia"
 apellido = "Roberts"
-#nombrecompleto = (nombre + " ", apellido) 
 nombrecompleto = ((nombre) + " " + str(apellido))
-
-print(nombrecompleto)
+print(nombrecompleto + "\n")
 
 #Practicas con Varables 3
 
 curso = "Python" 
-
 print("Estas tomando un curso de" + " " + str(curso))
+print("Estas tomando un curso de" + " " + curso + "\n")
 
-print("Estas tomando un curso de" + " " + curso)
+#Practica con Integers 
+
+num_entero = 7
+print(type(num_entero), "\n")
+
+#Practica con Floats 
+
+num_decimal = 3.141592653
+print(type(num_decimal), "\n")
+
+#Practica con Tipos de Datos Numericos 
+
+num1 = 7.5
+num2 = 2.5
+resultado = (num1 + num2)
+print(type(resultado), "\n")
+#Practica con Conversiones 1
+
+int(num1)
+print(type(num1), "\n")
+
+#practica de Conversiones 2
+
+float(num2)
+print(type(num2), "\n")
+
+#Practica con Conversiones 3
+print(type(num1 + num2), "\n")
