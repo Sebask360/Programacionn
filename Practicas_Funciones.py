9e04ae45d813ee3f20a5ff7a192adb0492459cde
Sebask360 <sebastiancusism@gmail.com>
2024-08-08 20:32:21 -0500

    Practicas

---
+++ Practicas_Funciones.py
@@ -0,0 +1,14 @@
+def sumar(num1, num2):
+
+    return num1 + num2
+
+resultado = sumar(10,20)
+print(resultado)
+
+def multiplicar(num1, num2):
+    
+    resultado =  num1 * num2   
+    return resultado
+
+resultado = multiplicar(5, 10)
+print(resultado)
---
+++ crear_funciones.py
@@ -0,0 +1,19 @@
+# Ejercicio 2
+
+def Bienvenida(nombre_persona):
+    
+    print(f"!Bienvenido {nombre_persona}!" )
+
+nombre_persona = input("Ingresa tu nombre: ")
+    
+Bienvenida(nombre_persona)
+
+#Ejercicio 3
+
+def cuadrado(numero):
+    print(f"El cuadrado de tu numero es: {n_cuadrado}")
+
+numero = int(input("Ingresa un numero del 1 al 9: "))
+n_cuadrado = (numero ** 2)
+
+cuadrado(n_cuadrado)
